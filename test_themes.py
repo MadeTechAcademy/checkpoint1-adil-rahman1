68d1bf87080efc67987707134bb11b0eb8210baa
Adil Rahman <arahman.99@outlook.com>
2025-07-16 15:31:04 +0100

    Refactor tests

--- test_themes.py
+++ test_themes.py
@@ -5,23 +5,17 @@ def test_13_duties_exist():
     assert len(duties) == 13
 
 def test_13_list_items_display_in_html():
-    no_of_list_items = 0
-
     with open('duties.html', 'r') as html_file:
-        lines_in_html_file = html_file.readlines()
+        file_content = html_file.read()
     
-    for line in lines_in_html_file:
-        if re.search("<li>.*</li>", line):
-            no_of_list_items += 1
-
-    assert no_of_list_items == 13
+    matches = re.findall("<li>.*</li>", file_content) 
+    assert len(matches) == 13
 
 def test_each_duty_exists_in_html():
     with open('duties.html', 'r') as html_file:
         file_content = html_file.read()
     
     matches = re.findall("<li>.*</li>", file_content) 
-    assert len(matches) == 13
 
     for duty in duties:
         assert f"<li>{duty}</li>" in matches
